14f5cc4de7457551412d6375919c29c0eafce813
antoinecamino <nova.clain@gmail.com>
2024-05-15 13:43:43 +0200

    indentation

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -16,46 +16,46 @@ class HomeController extends AbstractController
 {
     #[Route("/", name:"home", methods: ['GET', 'POST'] )] 
     public function home(Request $request, AvailabilityRepository $repository, CarRepository $carRepository): Response
-{
-    $dispos = $repository->findAll();
-    $cars = $carRepository->findAll();
-    
-    $form = $this->createForm(ChooseDateandPriceType::class);
-    $form->handleRequest($request);
-    
-    $startDate = null;
-    $endDate = null;
-    $maxPrice = null;
-
-    if ($form->isSubmitted() && $form->isValid()) {
-        $data = $form->getData(); 
-        $startDate = $data->getStartDate();
-        $endDate = $data->getEndDate();
-        $maxPrice = $data->getPrice();
-
+    {
+        $dispos = $repository->findAll();
+        $cars = $carRepository->findAll();
         
-        $availabilities = $repository->findByDateRange($startDate, $endDate);
-
+        $form = $this->createForm(ChooseDateandPriceType::class);
+        $form->handleRequest($request);
         
+        $startDate = null;
+        $endDate = null;
+        $maxPrice = null;
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData(); 
+            $startDate = $data->getStartDate();
+            $endDate = $data->getEndDate();
+            $maxPrice = $data->getPrice();
+
+            
+            $availabilities = $repository->findByDateRange($startDate, $endDate);
+
+            
+            return $this->render('home/index.html.twig', [
+                'dispos' => $availabilities,
+                'cars' => $cars,
+                'startDate' => $startDate,
+                'endDate' => $endDate,
+                'maxPrice' => $maxPrice, 
+                'ChooseDateandPriceType' => $form->createView()
+            ]);
+        }
+
         return $this->render('home/index.html.twig', [
-            'dispos' => $availabilities,
+            'dispos' => $dispos,
             'cars' => $cars,
             'startDate' => $startDate,
             'endDate' => $endDate,
-            'maxPrice' => $maxPrice, 
+            'maxPrice' => $maxPrice,
             'ChooseDateandPriceType' => $form->createView()
         ]);
     }
-
-    return $this->render('home/index.html.twig', [
-        'dispos' => $dispos,
-        'cars' => $cars,
-        'startDate' => $startDate,
-        'endDate' => $endDate,
-        'maxPrice' => $maxPrice,
-        'ChooseDateandPriceType' => $form->createView()
-    ]);
-}
 
 
 }
